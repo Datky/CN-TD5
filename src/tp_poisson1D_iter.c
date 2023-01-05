08ee017a876835625aad9e2b880071d4238884aa
Matéo Pasquier <iamsquel@gmail.com>
2023-01-05 10:54:25 +0100

    Ajout de la correction.

--- src/tp_poisson1D_iter.c
+++ src/tp_poisson1D_iter.c
@@ -1,5 +1,5 @@
 /******************************************/
-/* tp2_poisson1D_direct.c                 */
+/* tp2_poisson1D_iter.c                 */
 /* This file contains the main function   */
 /* to solve the Poisson 1D problem        */
 /******************************************/
@@ -7,35 +7,42 @@
 #include "atlas_headers.h"
 
 int main(int argc,char *argv[])
-/* ** argc: Nombre d'arguments */
-/* ** argv: Valeur des arguments */
+/* ** argc: Number of arguments */
+/* ** argv: Values of arguments */
 {
   int ierr;
   int jj;
   int nbpoints, la;
-  int ku, kl, kv, lab;
+  int ku, kl, lab, kv;
   int *ipiv;
   int info;
   int NRHS;
   double T0, T1;
-  double *RHS, *EX_SOL, *X;
-  double **AAB;
+  double *RHS, *SOL, *EX_SOL, *X;
   double *AB;
-
+  double *MB;
+  
   double temp, relres;
 
+  double opt_alpha;
+
+  /* Size of the problem */
   NRHS=1;
-  nbpoints=10;
+  nbpoints=12;
   la=nbpoints-2;
-  T0=-5.0;
-  T1=5.0;
+
+  /* Dirichlet Boundary conditions */
+  T0=5.0;
+  T1=20.0;
 
   printf("--------- Poisson 1D ---------\n\n");
   RHS=(double *) malloc(sizeof(double)*la);
+  SOL=(double *) calloc(la, sizeof(double)); 
   EX_SOL=(double *) malloc(sizeof(double)*la);
   X=(double *) malloc(sizeof(double)*la);
 
-  // TODO : you have to implement those functions
+  /* Setup the Poisson 1D problem */
+  /* General Band Storage */
   set_grid_points_1D(X, &la);
   set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
   set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
@@ -44,49 +51,60 @@ int main(int argc,char *argv[])
   write_vec(EX_SOL, &la, "EX_SOL.dat");
   write_vec(X, &la, "X_grid.dat");
 
-  kv=1;
+  kv=0;
   ku=1;
   kl=1;
   lab=kv+kl+ku+1;
-
+  
   AB = (double *) malloc(sizeof(double)*lab*la);
-
   set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-
+  
+  /* uncomment the following to check matrix A */
   // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
-
-  printf("Solution with LAPACK\n");
-  /* LU Factorization */
-  info=0;
-  ipiv = (int *) calloc(la, sizeof(int));
-  dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
-
-  /* LU for tridiagonal matrix  (can replace dgbtrf_) */
-  // ierr = dgbtrftridiag(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
-
-  // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
   
-  /* Solution (Triangular) */
-  if (info==0){
-    dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info, 1);
-    if (info!=0){printf("\n INFO DGBTRS = %d\n",info);}
-  }else{
-    printf("\n INFO = %d\n",info);
-  }
+  /********************************************/
+  /* Solution (Richardson with optimal alpha) */
 
-  /* It can also be solved with dgbsv */
-  // TODO : use dgbsv
+  /* Computation of optimum alpha */
+  opt_alpha = richardson_alpha_opt(&la);
+  printf("Optimal alpha for simple Richardson iteration is : %lf",opt_alpha); 
 
-  write_xy(RHS, X, &la, "SOL.dat");
+  /* Solve */
+  double tol=1e-3;
+  int maxit=1000;
+  double *resvec;
+  int nbite=0;
 
-  /* Relative forward error */
-  // TODO : Compute relative norm of the residual
+  resvec=(double *) calloc(maxit, sizeof(double));
+
+  /* Solve with Richardson alpha */
+  richardson_alpha(AB, RHS, SOL, &opt_alpha, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+
+  /* Richardson General Tridiag */
+
+  /* get MB (:=M, D for Jacobi, (D-E) for Gauss-seidel) */
+  kv = 1;
+  ku = 1;
+  kl = 1;
+  MB = (double *) malloc(sizeof(double)*(lab)*la);
+  // extract_MB_jacobi_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
+  // extract_MB_gauss_seidel_tridiag(AB, MB, &lab, &la, &ku, &kl, &kv);
   
-  printf("\nThe relative forward error is relres = %e\n",relres);
+  /* Solve with General Richardson */
+  // richardson_MB(AB, RHS, SOL, MB, &lab, &la, &ku, &kl, &tol, &maxit, resvec, &nbite);
+  
+  /* Write solution */
+  write_vec(SOL, &la, "SOL.dat");
 
+  /* Write convergence history */
+  write_vec(resvec, &nbite, "RESVEC.dat");
+
+  free(resvec);
   free(RHS);
+  free(SOL);
   free(EX_SOL);
   free(X);
   free(AB);
+  free(MB);
   printf("\n\n--------- End -----------\n");
 }
